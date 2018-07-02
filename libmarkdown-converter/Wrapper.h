ab02f514431ea6de7fe70681c82e06e359aa78c9
leixiaoda <leixiaoda@gmail.com>
2018-07-02 18:03:50 +0800

    style: change name of creator

--- libmarkdown-converter/Wrapper.h
+++ libmarkdown-converter/Wrapper.h
@@ -2,7 +2,7 @@
 //  Wrapper.h
 //  SwiftPlusCPP
 //
-//  Created by bytedance on 2018/6/15.
+//  Created by Radar on 2018/6/15.
 //  Copyright © 2018年 radar. All rights reserved.
 //
 
--- libmarkdown-converter/libmarkdown.swift
+++ libmarkdown-converter/libmarkdown.swift
@@ -2,7 +2,7 @@
 //  libmarkdown.swift
 //  libmarkdown
 //
-//  Created by bytedance on 2018/6/26.
+//  Created by Radar on 2018/6/26.
 //  Copyright © 2018年 radar. All rights reserved.
 //
 
